e52c6bcc6e7af68021c33e096186263b4b2f0672
Başar Kaya <kayaism@gmail.com>
2018-04-27 08:55:48 +0300

    Add project files.

---
+++ ExamTest1/Controllers/ExamController.cs
@@ -0,0 +1,140 @@
+﻿using ExamTest1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamTest1.Controllers
+{
+
+    public class ExamController : Controller
+    {
+        IEnumerable<Question> questions;
+
+        List<Question> results;
+        public ExamController()
+        {
+            Question question = new Question();
+            questions = question.GetQuestions().OrderBy(i => i.Id);
+
+            results = new List<Question>();
+        }
+
+        public ActionResult Index()
+        {           
+            
+            return View(questions.ToList()[0]);
+        }
+
+        //public ActionResult Index(int? qNumber, int id, string Q1)
+
+        [HttpPost]
+        public ActionResult Index(Question question)
+        {
+            var qNumber = question.Id;
+            //int number = qNumber ?? 1;
+            //if (qNumber.HasValue)
+            //    number = qNumber.Value + 1;
+
+            if (qNumber == 5)
+            {
+                //return Content("");
+                return RedirectToAction("Finish",  results );
+            }
+            var nextQuestion = questions.FirstOrDefault(i => i.Id == qNumber+1);
+
+            results.Add(question);
+            //results.Add(id, Q1);
+
+            //Q1 = "";
+
+            //qNumber = qNumber + 1;
+            return View(nextQuestion);
+        }
+
+        // GET: Exam/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Exam/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Finish(List<Question> dictionary)
+        {
+            string str = "";
+            foreach (var item in dictionary)
+            {
+                str += item.Id.ToString();
+            }
+
+            return Content(str + "\nSınav Bitti.");
+        }
+
+        // POST: Exam/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Exam/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Exam/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Exam/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Exam/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
---
+++ ExamTest1/Models/Question.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamTest1.Models
+{
+    public class Question
+    {
+        public Question()
+        {
+
+        }
+
+        public Question(string text, string sectionA, string sectionB, string sectionC, string correctAnswer)
+        {
+            this.Text = text;
+            this.SectionA = sectionA;
+            this.SectionB = sectionB;
+            this.SectionC = sectionC;
+            this.CorrectAnswer = correctAnswer;
+        }
+
+        public int Id { get; set; }
+
+
+        public string Text { get; set; }
+
+        public string SectionA { get; set; }
+
+        public string SectionB { get; set; }
+
+        public string SectionC { get; set; }
+
+        public bool boolSectionA { get; set; }
+        public bool boolSectionB { get; set; }
+        public bool boolSectionC { get; set; }
+
+        public string CorrectAnswer { get; set; }
+
+        public List<Question> GetQuestions()
+        {
+            List<Question> questions = new List<Question>();
+            questions.Add(new Question() { Id = 1, Text = "Türkiyenin başkenti neresidir?", SectionA = "İzmir", SectionB = "İstanbul", SectionC = "Ankara", CorrectAnswer = "Ankara" });
+            questions.Add(new Question() { Id = 2, Text = "Almanya başkenti neresidir?", SectionA = "Berlin", SectionB = "İstanbul", SectionC = "Ankara", CorrectAnswer = "Berlin" });
+            questions.Add(new Question() { Id = 3, Text = "Fransa başkenti neresidir?", SectionA = "İzmir", SectionB = "Paris", SectionC = "Ankara", CorrectAnswer = "Paris" });
+            questions.Add(new Question() { Id = 4, Text = "Amerika başkenti neresidir?", SectionA = "İzmir", SectionB = "İstanbul", SectionC = "Dc Washington", CorrectAnswer = "Dc Washington" });
+            questions.Add(new Question() { Id = 5, Text = "Vietnam başkenti neresidir?", SectionA = "İzmir", SectionB = "Honeo", SectionC = "Ankara", CorrectAnswer = "Honeo" });
+
+
+            return questions;
+
+
+        }
+
+
+    }
+}
